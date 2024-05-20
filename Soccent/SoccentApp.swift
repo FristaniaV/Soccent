c5c784778f452ecbb28bb9d9304fbb530dd086cc
Fristania Verenita <fristaniaverenita@gmail.com>
2024-05-20 14:30:30 +0700

    Initial Commit

---
+++ Soccent/SoccentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccentApp.swift
+//  Soccent
+//
+//  Created by Fristania Verenita on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SoccentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
